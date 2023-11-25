8f0cfa06a2d7aa3291ee9eef263da4d17b87d7e3
Julien Bourdeau <julien@julienbourdeau.com>
2023-11-25 22:14:39 +0100

    add copy query button

--- client/src/components/panels/QueriesPanel.vue
+++ client/src/components/panels/QueriesPanel.vue
@@ -4,12 +4,27 @@ import type { BackendRequest } from "@/models/Request.ts"
 const props = defineProps<{
   currentRequest: BackendRequest
 }>()
+
+function copyToClipboard(text: string) {
+  const type = "text/plain"
+  const blob = new Blob([text], { type })
+  const data = [new ClipboardItem({ [type]: blob })]
+  navigator.clipboard.write(data)
+}
 </script>
 
 <template>
   <div class="flex flex-col space-y-8">
     <div v-for="query in props.currentRequest.queries" class="space-y-3">
-      <div class="font-bold">{{ query.name }}</div>
+      <div class="font-bold">
+        {{ query.name }}
+        <span
+          @click="copyToClipboard(query.sql)"
+          class="px-3 text-xs uppercase text-stone-400 cursor-pointer"
+          title="Copy SQL query to clipboard"
+          >copy</span
+        >
+      </div>
       <div class="">
         <highlightjs language="sql" :code="query.sql" />
       </div>
