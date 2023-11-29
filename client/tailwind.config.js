5f54c4e9364cc1e444dcfa6126650f41ebda0b67
Julien Bourdeau <julien@julienbourdeau.com>
2023-11-29 18:46:07 +0100

    experiement with styles lol

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   prefix: "bug-",
+  important: true,
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
