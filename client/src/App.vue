b1aa30e5f64480d1a93f3271e05ea212e3f7759e
Julien Bourdeau <julien@julienbourdeau.com>
2023-12-17 10:57:45 +0100

    IT WORKS

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import Debugbar from "@/Debugbar.vue"
+import { ShadowRoot } from "vue-shadow-dom"
 </script>
 
 <template>
-  <debugbar></debugbar>
+  <div style="background: #22863a">
+    <shadow-root id="mega-uniq-id">
+      <debugbar></debugbar>
+    </shadow-root>
+  </div>
 </template>
