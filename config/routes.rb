ca43ef382779a5cb75a4ed78b8e24907b6f76034
Julien Bourdeau <julien@julienbourdeau.com>
2024-02-18 14:13:42 +0100

    assets/style route was removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Debugbar::Engine.routes.draw do
   mount ActionCable.server => '/cable'
 
-  # TODO: Silence logs for these routes if `::Rails.application.config.assets.quiet` is true
-  get 'assets/style' => "assets#css", format: :css
+  # TODO: Silence logs for this route if `::Rails.application.config.assets.quiet` is true
   get 'assets/script' => "assets#js"
 end
