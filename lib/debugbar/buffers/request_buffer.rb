5bdd6e55774ee31639ab37ab5f84dee7c5e3d5ae
Julien Bourdeau <julien@julienbourdeau.com>
2024-02-26 11:54:50 +0100

    I think this feature was unused

--- lib/debugbar/buffers/request_buffer.rb
+++ lib/debugbar/buffers/request_buffer.rb
@@ -2,22 +2,7 @@ module Debugbar
   class RequestBuffer
     class << self
       def init(adapter)
-        @enabled = false
         @adapter = adapter
-      end
-
-      def enable!
-        @enabled = true
-        send_all
-      end
-
-      def disable!
-        @enabled = false
-      end
-
-      def send_all
-        data = @collection.values.map(&:to_h)
-        ActionCable.server.broadcast("debugbar_channel", data)
       end
 
       %w(push each all remove clear!).each do |name|
