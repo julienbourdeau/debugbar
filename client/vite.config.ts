7f981caac048ef16ac6a3185358d20fdd277c856
Julien Bourdeau <julien@julienbourdeau.com>
2024-02-26 14:56:19 +0100

    Fix "[Vue warn]: Failed to resolve component: cc:Work"
    The ruby logo is embeded as a vue component because I want it to be part of the single js file, not an external asset.
    It has a lot of unusual xml tags and it confuses Vue
    
    [Vue warn]: Failed to resolve component: dc:type
    [Vue warn]: Failed to resolve component: cc:Work
    [Vue warn]: Failed to resolve component: dc:format

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -24,7 +24,17 @@ export default defineConfig({
   define: {
     __VUE_PROD_DEVTOOLS__: isNotReallyProd(),
   },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // This is because I embed the ruby logo SVG as a vue component.
+          // There is probably a better way.
+          isCustomElement: (tag) => tag.includes(":"),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
