a2a9c1135f69127f2285a5e4eb1e64ff646628fe
Julien Bourdeau <julien@julienbourdeau.com>
2024-02-27 16:52:49 +0100

    Client now automatically call 127.0.0.1:3000 when using DEV mode

--- client/src/AppDev.vue
+++ client/src/AppDev.vue
@@ -3,6 +3,7 @@ import Debugbar from "@/Debugbar.vue"
 import { onMounted } from "vue"
 import { useRequestsStore } from "@/stores/RequestsStore.ts"
 import { BackendRequestData } from "@/models/Request.ts"
+import { random } from "lodash"
 
 onMounted(() => {
   if (import.meta.env.DEV && import.meta.env.VITE_LOAD_FIXTURES) {
@@ -12,6 +13,18 @@ onMounted(() => {
       .pop()
     useRequestsStore().setCurrentRequestById(lastId)
   }
+
+  setTimeout(() => {
+    // We use `no-cors` mode because we don't need the response, we just want to trigger the request,
+    // the debugbar will receive the information related to these requests via websocket
+    fetch("http://127.0.0.1:3000/post-list", { mode: "no-cors" })
+    fetch("http://127.0.0.1:3000/random", { mode: "no-cors" })
+    fetch("http://127.0.0.1:3000/api/jobs?name=refresh&post_id=" + random(1, 300), { mode: "no-cors" })
+    // fetch("http://127.0.0.1:3000/api/jobs?name=send_email&post_id=" + random(1, 300), { mode: "no-cors" })
+    fetch("http://127.0.0.1:3000/api/errors?code=404", { mode: "no-cors" })
+    // fetch("http://127.0.0.1:3000/api/errors?code=401", { mode: "no-cors" })
+    fetch("http://127.0.0.1:3000/api/errors?code=500", { mode: "no-cors" })
+  }, 600)
 })
 </script>
 
