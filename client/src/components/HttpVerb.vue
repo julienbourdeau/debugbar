266a1ea1ceed5aad921ec3dff9c578e2b20701c3
Julien Bourdeau <julien@julienbourdeau.com>
2025-01-01 04:34:46 +0100

    wip

---
+++ client/src/components/HttpVerb.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed, onMounted } from "vue"
+
+const props = defineProps<{
+  verb: string
+}>()
+</script>
+
+<template>
+  <div
+    class="text-white font-medium px-1.5 py-1 rounded text-xs"
+    :class="{
+      'bg-sky-600': props.verb.toLowerCase() == 'get',
+      'bg-indigo-600': props.verb.toLowerCase() == 'post',
+      'bg-purple-500': props.verb.toLowerCase() == 'put',
+      'bg-violet-400': props.verb.toLowerCase() == 'patch',
+      'bg-red-700': props.verb.toLowerCase() == 'delete',
+    }"
+    v-text="props.verb.toUpperCase()"
+  ></div>
+</template>
---
+++ client/src/components/RequestListItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { BackendRequest } from "@/models/Request.ts"
+
+const props = defineProps<{
+  request: BackendRequest
+}>()
+</script>
+
+<template>
+  <div>{{ props.request.pathWithVerb }}</div>
+</template>
