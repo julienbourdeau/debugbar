cbc8bfd75e58be37f705b50d44d22b64a8d93c4c
Julien Bourdeau <julien@julienbourdeau.com>
2025-01-03 18:34:49 +0100

    small refactor

--- client/src/AppDevtoolsDev.vue
+++ client/src/AppDevtoolsDev.vue
@@ -10,11 +10,39 @@ onMounted(() => {
     fetch("http://127.0.0.1:3000/post-list", { mode: "no-cors" })
     fetch("http://127.0.0.1:3000/random", { mode: "no-cors" })
     fetch("http://127.0.0.1:3000/api/jobs?name=refresh&post_id=" + random(1, 300), { mode: "no-cors" })
-    // fetch("http://127.0.0.1:3000/api/jobs?name=send_email&post_id=" + random(1, 300), { mode: "no-cors" })
-    fetch("http://127.0.0.1:3000/api/errors?code=404", { mode: "no-cors" })
+    fetch("http://127.0.0.1:3000/api/jobs?name=send_email&post_id=" + random(1, 300), { mode: "no-cors" })
+    // fetch("http://127.0.0.1:3000/api/errors?code=404", { mode: "no-cors" })
     // fetch("http://127.0.0.1:3000/api/errors?code=401", { mode: "no-cors" })
-    fetch("http://127.0.0.1:3000/api/errors?code=500", { mode: "no-cors" })
-  }, 600)
+    // fetch("http://127.0.0.1:3000/api/errors?code=500", { mode: "no-cors" })
+    fetch("http://127.0.0.1:3000/api/other_methods", {
+      mode: "no-cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: "{yolo: true}",
+    })
+    fetch("http://127.0.0.1:3000/api/other_methods", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: "{yolo: true}",
+    })
+    fetch("http://127.0.0.1:3000/api/other_methods", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: "{yolo: true}",
+    })
+    fetch("http://127.0.0.1:3000/api/other_methods", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  }, 800)
 })
 </script>
 
--- client/src/components/ui/HttpVerb.vue
+++ client/src/components/ui/HttpVerb.vue
@@ -6,13 +6,13 @@ const props = defineProps<{
 
 <template>
   <div
-    class="px-1 py-0.5 rounded text-xs text-white"
+    class="px-1 py-0.5 rounded text-xs text-white bg-slate-700"
     :class="{
-      'bg-sky-700': props.verb.toLowerCase() == 'get',
-      'bg-indigo-700': props.verb.toLowerCase() == 'post',
-      'bg-purple-700': props.verb.toLowerCase() == 'put',
-      'bg-violet-500': props.verb.toLowerCase() == 'patch',
-      'bg-red-700': props.verb.toLowerCase() == 'delete',
+      '!bg-sky-700': props.verb.toLowerCase() == 'get',
+      '!bg-indigo-700': props.verb.toLowerCase() == 'post',
+      '!bg-purple-700': props.verb.toLowerCase() == 'put',
+      '!bg-violet-500': props.verb.toLowerCase() == 'patch',
+      '!bg-red-700': props.verb.toLowerCase() == 'delete',
     }"
     v-text="props.verb.toUpperCase()"
   ></div>
