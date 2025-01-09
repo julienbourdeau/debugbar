999aa536996b8b2830cd9dd6f157dd5718dcd6fd
Julien Bourdeau <julien@julienbourdeau.com>
2025-01-09 09:25:57 +0100

    track all requests made to current tab 😎

--- client/extension/manifest.ts
+++ client/extension/manifest.ts
@@ -5,15 +5,15 @@ const manifest: chrome.runtime.Manifest = {
   // options_page: "extension/options.html",
   description: "Get a better understanding of your application performance and behavior with the debugbar.",
   icons: {
-    16: "extension/icons/debugbar-icon-16.png",
+    // 16: "extension/icons/debugbar-icon-16.png",
     48: "extension/icons/ror-icon-48.png",
     128: "extension/icons/ror-icon-128.png",
   },
   manifest_version: 3,
   name: "Debugbar for Ruby on Rails",
-  permissions: ["scripting", "webRequest", "activeTab"],
+  permissions: ["scripting", "webRequest", "activeTab", "tabs"],
   devtools_page: "extension/devtools.html",
-  host_permissions: ["http://localhost:3000/*", "http://127.0.0.1:3000/*", "ws://127.0.0.1:3000/*"],
+  host_permissions: ["<all_urls>", "http://localhost:3000/*", "http://127.0.0.1:3000/*", "ws://127.0.0.1:3000/*"],
   version: "0.6.1",
 }
 
--- client/public/devtools.js
+++ client/public/devtools.js
@@ -1,11 +1,47 @@
-var _browser
-if (chrome) {
-  _browser = chrome
-} else {
-  _browser = browser
+// var _browser
+// if (chrome) {
+//   _browser = chrome
+// } else {
+//   _browser = browser
+// }
+// _browser.devtools.panels.create(
+//   "Debugbar", // title
+//   "todo.png", // icon
+//   "extension/devtools.html" // content
+// )
+
+// Create a panel in DevTools
+chrome.devtools.panels.create("Request Monitor", null, "extension/devtools.html", function (panel) {
+  console.log("Panel created")
+})
+
+// Create a background page connection
+const backgroundPageConnection = chrome.runtime.connect({
+  name: "devtools-page",
+})
+
+// Listen to network requests
+chrome.devtools.network.onRequestFinished.addListener((request) => {
+  const requestData = {
+    url: request.request.url,
+    method: request.request.method,
+    status: request.response.status,
+    timestamp: new Date().toISOString(),
+  }
+
+  addRequestToList(requestData)
+})
+
+function addRequestToList(requestData) {
+  const list = document.getElementById("requests-list")
+  const listItem = document.createElement("li")
+  listItem.className = "request-item"
+  listItem.innerHTML = `
+        <strong>${requestData.method}</strong> ${requestData.status}
+        <br>
+        ${requestData.url}
+        <br>
+        <small>${requestData.timestamp}</small>
+    `
+  list.insertBefore(listItem, list.firstChild)
 }
-_browser.devtools.panels.create(
-  "Debugbar", // title
-  "todo.png", // icon
-  "extension/devtools.html" // content
-)
